91dbafb1b55f4fec8b86aa7d0347dc506a9fa1c6
joaquin-medina <joaquinmedinadev@gmail.com>
2023-10-18 10:22:46 -0300

    Relacion Muchos a Muchos

--- WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/Entidades/Autor.cs
@@ -9,6 +9,7 @@ namespace WebApiAutores.Entidades
         [Required(ErrorMessage = "El campo {0} es requerido.")]
         [StringLength(maximumLength: 120, ErrorMessage = "El campo {0} no debe de tener mas de {1} carácteres.")]
         [PrimeraLetraMayuscula]
-        public string Nombre { get; set; }     
+        public string Nombre { get; set; }
+        public List<AutorLibro> AutoresLibros { get; set; }
     }
 }
